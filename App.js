52f09b3371d1519372325bd593becab44b011621
basantimages <124242939+basantimages@users.noreply.github.com>
2023-03-05 15:45:35 +0530

    Cancel retrying button bug removed

---
+++ App.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [intervalId, setIntervalId] = useState(null);
+  const [addInterval, setAddInterval] = useState(false);
+
+  useEffect(() => {
+    if (addInterval) {
+      const interval = setInterval(fetchMoviesHandler, 5000);
+      setIntervalId(interval);
+    }
+  }, [addInterval]);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something went wrong...Retrying!");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+      setAddInterval(true);
+    }
+    setIsLoading(false);
+  }
+
+  function cancelInterval() {
+    clearInterval(intervalId);
+    setIntervalId(null);
+    setError("Retrying cancelled!");
+    setAddInterval(false);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={cancelInterval}>Cancel Retrying</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
